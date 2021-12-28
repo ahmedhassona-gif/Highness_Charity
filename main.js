7ecec40b3d4291c0394a3713bce4e9367812b32e
ahmedhassona-gif <85953448+ahmedhassona-gif@users.noreply.github.com>
2021-12-28 11:18:43 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+$(function (){
+   'use strict'
+   var winh = $(window).height(),
+       upperh = $('.upper-bar').innerHeight(),
+       navh = $('.navbar').innerHeight();
+   $('.slider,.carousel-item').height(winh - ( upperh + navh ));
+
+   // Featured work shuffle
+
+   $('.features-work ul li').on('click', function() {
+     $(this).addClass('active').siblings().removeClass('active'); 
+     if($(this).data('class') === 'all'){
+         $('.shuffle-imgs .col-md').css('opacity',1);
+     }
+
+     else{
+         $('.shuffle-imgs .col-md').css('opacity','.08');
+         $($(this).data('class')).parent().css('opacity',1);
+     }
+
+   });
+
+});
